64e4c11f22880002b16a0ce388952075b40281cf
victorynox <it.professor02@gmail.com>
2018-02-19 13:32:42 +0200

    Fix dataTime usage

--- src/DataStore/src/DataStore/Interfaces/DateTimeInterface.php
+++ src/DataStore/src/DataStore/Interfaces/DateTimeInterface.php
@@ -3,8 +3,6 @@
 
 namespace rollun\datastore\DataStore\Interfaces;
 
-use rollun\datastore\DataStore\Iterators\DataStoreIterator;
-
 interface DateTimeInterface extends DataStoresInterface
 {
     const FIELD_DATETIME = "datetime";
