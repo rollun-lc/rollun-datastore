095883e65656c237feac40224cdcf2305ef41fd7
Andrii <lasgrate@gmail.com>
2018-12-04 12:33:20 +0200

    remove config cache

--- config/config.php
+++ config/config.php
@@ -5,12 +5,6 @@ use Zend\ConfigAggregator\ArrayProvider;
 use Zend\ConfigAggregator\ConfigAggregator;
 use Zend\ConfigAggregator\PhpFileProvider;
 
-// To enable or disable caching, set the `ConfigAggregator::ENABLE_CACHE` boolean in
-// `config/autoload/local.php`.
-$cacheConfig = [
-    'config_cache_path' => 'data/config-cache.php',
-];
-
 // Make environment variables stored in .env accessible via getenv(), $_ENV or $_SERVER.
 (new Dotenv())->load('.env');
 
@@ -38,7 +32,6 @@ $aggregator = new ConfigAggregator([
     //   - `local.dev.php`,    `local.test.php`,     `prod.local.prod.php`
     //   - `*.local.dev.php`,  `*.local.test.php`,  `*.prod.local.prod.php`
     new PhpFileProvider(realpath(__DIR__) . "/autoload/{{,*.}global.{$appEnv},{,*.}local.{$appEnv}}.php"),
-], $cacheConfig['config_cache_path']);
-
+]);
 
 return $aggregator->getMergedConfig();
