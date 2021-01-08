b06be1c241cd6890342082dccd496fc6a666bccd
misha-rollun <zuiev.rollun@gmail.com>
2021-01-08 12:37:48 +0200

    Disable deprecated error reporting

--- public/test.php
+++ public/test.php
@@ -13,6 +13,8 @@ use Zend\Diactoros\Response;
 use Zend\Diactoros\ServerRequest;
 use Zend\Stratigility\MiddlewarePipe;
 
+error_reporting(E_ALL ^ E_USER_DEPRECATED);
+
 // Delegate static file requests back to the PHP built-in webserver
 if (php_sapi_name() === 'cli-server'
     && is_file(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
