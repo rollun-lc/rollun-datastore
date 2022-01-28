6ff67098e21f8b9d066baa0e807b7255dd3baf78
Zuiev Mykhailo <zuiev.rollun@gmail.com>
2022-01-28 12:47:00 +0200

    test stringFormatter: create tests

---
+++ test/unit/DataStore/DataStore/Formatter/StringFormatterTest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace rollun\test\unit\DataStore\DataStore\Formatter;
+
+use rollun\datastore\DataStore\Formatter\FormatterInterface;
+use rollun\datastore\DataStore\Formatter\StringFormatter;
+
+class StringFormatterTest extends TestCase
+{
+    public function dataProvider(): array
+    {
+        return [
+            'Convert string' => [$str = uniqid(), $str],
+            'Convert int' => [123, '123'],
+            'Convert float' => [12.345, '12.345'],
+            'Convert bool false' => [false, 'false'],
+            'Convert bool true' => [true, 'true'],
+            'Convert associative array' => [['key' => 'value'], '{"key":"value"}'],
+            'Convert list array' => [['str', 123, 1.23], '["str",123,1.23]']
+        ];
+    }
+
+    protected function getFormatter(): FormatterInterface
+    {
+        return new StringFormatter();
+    }
+}
