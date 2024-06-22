2293f79095e236d1088ecb3999ce6d9b001e253c
Mykhailo Zuiev <zuiev.rollun@gmail.com>
2024-06-22 16:12:26 +0200

    fix(KzNUCYHy): connection exception is not detected

--- src/DataStore/src/DataStore/LaminasDbExceptionDetector.php
+++ src/DataStore/src/DataStore/LaminasDbExceptionDetector.php
@@ -14,9 +14,9 @@ class LaminasDbExceptionDetector
         }
         if (
             // Exception message from a PDO driver: Laminas\Db\Adapter\Driver\Pdo\Connection
-            str_starts_with('Connect Error:', $e->getMessage()) ||
+            str_starts_with($e->getMessage(), 'Connect Error:') ||
             // Exception message from a Mysqli driver: Laminas\Db\Adapter\Driver\Mysqli\Connection
-            str_starts_with('Connection error', $e->getMessage())
+            str_starts_with($e->getMessage(), 'Connection error')
         ) {
             return true;
         }
