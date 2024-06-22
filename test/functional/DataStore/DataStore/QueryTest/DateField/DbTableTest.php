d3178345c893a82a769174eab9dfb47b12a9767d
Mykhailo Zuiev <zuiev.rollun@gmail.com>
2024-06-22 15:48:07 +0200

    refactor(KzNUCYHy): move datastore creation to DbTableFactory in tests

--- test/functional/DataStore/DataStore/QueryTest/DateField/DbTableTest.php
+++ test/functional/DataStore/DataStore/QueryTest/DateField/DbTableTest.php
@@ -2,70 +2,36 @@
 
 namespace rollun\test\functional\DataStore\DataStore\QueryTest\DateField;
 
-use Laminas\Db\Adapter\Adapter;
-use Laminas\Db\TableGateway\TableGateway;
-use rollun\datastore\DataStore\DbTable;
 use rollun\datastore\DataStore\Interfaces\DataStoreInterface;
 use rollun\datastore\TableGateway\TableManagerMysql;
+use rollun\test\functional\DataStore\DataStore\DbTableFactory;
 
 class DbTableTest extends BaseTest
 {
-    /**
-     * @var DbTable
-     */
-    private $dbTable;
-
-    /**
-     * @var TableManagerMysql
-     */
-    private $mysqlManager;
-
-
     protected function getDataStore(): DataStoreInterface
     {
-        if ($this->dbTable === null) {
-            $this->dbTable = $this->setUpDbTable();
-        }
-        return $this->dbTable;
+        return $this->getDbTableFactory()->create();
     }
 
     protected function tearDown(): void
     {
         parent::tearDown();
-        $this->getMysqlManager()->deleteTable(self::TABLE_NAME);
+        $this->getDbTableFactory()->delete();
     }
 
-    private function setUpDbTable(): DbTable
+    private function getDbTableFactory(): DbTableFactory
     {
-        $mysqlManager = $this->getMysqlManager();
-
-        if ($mysqlManager->hasTable(self::TABLE_NAME)) {
-            $mysqlManager->deleteTable(self::TABLE_NAME);
-        }
-
-        $mysqlManager->createTable(self::TABLE_NAME, [
-            self::ID_NAME => [
-                'field_type' => TableManagerMysql::TYPE_INTEGER,
+        return new DbTableFactory(
+            adapter: $this->getContainer()->get('db'),
+            tableName: self::TABLE_NAME,
+            tableConfig: [
+                self::ID_NAME => [
+                    'field_type' => TableManagerMysql::TYPE_INTEGER,
+                ],
+                self::FIELD_NAME => [
+                    'field_type' => TableManagerMysql::TYPE_DATETIME,
+                ],
             ],
-            self::FIELD_NAME => [
-                'field_type' => TableManagerMysql::TYPE_DATETIME,
-            ],
-        ]);
-        $tableGateway = new TableGateway(self::TABLE_NAME, $this->getDbAdapter());
-
-        return new DbTable($tableGateway);
-    }
-
-    private function getMysqlManager(): TableManagerMysql
-    {
-        if ($this->mysqlManager === null) {
-            $this->mysqlManager = new TableManagerMysql($this->getDbAdapter());
-        }
-        return $this->mysqlManager;
-    }
-
-    private function getDbAdapter(): Adapter
-    {
-        return $this->getContainer()->get('db');
+        );
     }
 }
