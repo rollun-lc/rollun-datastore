175df18183948e0c70e33552965b431e3208b7b6
Mykhailo Zuiev <zuiev.rollun@gmail.com>
2025-05-06 14:01:36 +0200

    fix(c6CRG2Sb): 'Invalid numeric literal' when run equals query with number as string with leading zero

--- test/functional/DataStore/DataStore/MemoryTest.php
+++ test/functional/DataStore/DataStore/MemoryTest.php
@@ -6,10 +6,8 @@ namespace rollun\test\functional\DataStore\DataStore;
 
 use PHPUnit\Framework\TestCase;
 use rollun\datastore\DataStore\Memory;
-use rollun\datastore\Rql\Node\AlikeNode;
 use rollun\datastore\Rql\Node\ContainsNode;
-use rollun\datastore\Rql\Node\LikeGlobNode;
-use Xiag\Rql\Parser\Node\Query\ScalarOperator\LikeNode;
+use Xiag\Rql\Parser\Node\Query\ScalarOperator\EqNode;
 use Xiag\Rql\Parser\Query;
 
 class MemoryTest extends TestCase
@@ -83,6 +81,22 @@ class MemoryTest extends TestCase
 
         $query = new Query();
         $query->setQuery(new ContainsNode('name', 'hello'));
+        $result = $object->query($query);
+
+        $this->assertNotEmpty($result);
+    }
+
+    public function testIntWithLeadingZeros()
+    {
+        $item = [
+            'id' => 1,
+            'name' => '094057',
+        ];
+        $object = new Memory();
+        $object->create($item);
+
+        $query = new Query();
+        $query->setQuery(new EqNode('name', '094057'));
         $result = $object->query($query);
 
         $this->assertNotEmpty($result);
