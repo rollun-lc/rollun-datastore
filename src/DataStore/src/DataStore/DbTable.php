04207cc1262ee8aa3dc9f236c0bd3eddd2c29f67
vadymk-rollun <kopylovvadym.rollun@gmail.com>
2025-08-18 10:57:14 +0000

    feat(ObrQEXJE): update DbTable.php - check PRI in body in queriedUpdate & fix tests related to this

--- src/DataStore/src/DataStore/DbTable.php
+++ src/DataStore/src/DataStore/DbTable.php
@@ -281,6 +281,11 @@ class DbTable extends DataStoreAbstract
             throw new InvalidArgumentException('Queried update does not support select or groupBy.');
         }
 
+        $identifier = $this->getIdentifier();
+        if (array_key_exists($identifier, $record)) {
+            throw new InvalidArgumentException("Primary key '$identifier' must not be present in queriedUpdate body.");
+        }
+
 
         // prepare record
         foreach ($record as $k => $v) {
@@ -308,7 +313,7 @@ class DbTable extends DataStoreAbstract
             $update->set($record);
 
             $where = new Where();
-            $where->in($this->getIdentifier(), $selectedIds);
+            $where->in($identifier, $selectedIds);
             $update->where($where);
 
             $sqlString = $sql->buildSqlString($update);
